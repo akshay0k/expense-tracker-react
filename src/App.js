7f69f270c5bd92aca42a971e5cf758166e25aceb
Akshay K <sb38617214@gmail.com>
2023-10-25 23:13:39 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import AddTransaction from './components/AddTransaction';
+import { Balance } from './components/Balance';
 import Header from './components/Header';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
 function App() {
   return (
     <div >
       <Header/>
+      <div className='container'>
+        <Balance/> 
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
     </div>
   );
 }
